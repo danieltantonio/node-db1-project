57f5728eb167e334faa79a0fad3a7bde760197cc
Daniel Antonio <dernlsenpai@gmail.com>
2020-10-05 13:07:08 -0700

    Created SQLQueries, created READ operation and endpoint

--- api/server.js
+++ api/server.js
@@ -6,4 +6,29 @@ const server = express();
 
 server.use(express.json());
 
+server.get('/', (req,res) => {
+    db
+    .select('*')
+    .from('accounts')
+    .then(accounts => {
+        res.status(400).json(accounts)
+    })
+    .catch(err => {
+        res.status(500).json(err)
+    })
+});
+
+server.get('/:id', (req,res) => {
+    db
+    .select('*')
+    .from('accounts')
+    .where('id', req.params.id)
+    .then(acc => {
+        res.status(200).json(acc)
+    })
+    .catch(err => {
+        res.status(500).json(err)
+    })
+});
+
 module.exports = server;
